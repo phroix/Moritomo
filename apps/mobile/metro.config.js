dfe598901bb67a74f1a34ce4257da74c35b1faff
phroix <94416893+phroix@users.noreply.github.com>
2025-09-19 17:30:56 +0200

    initial commit

---
+++ apps/mobile/metro.config.js
@@ -0,0 +1,19 @@
+// apps/mobileApp/metro.config.js
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
+
+/**
+ * Metro configuration
+ * <https://reactnative.dev/docs/metro>
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  resolver: {
+    unstable_enableSymlinks: true, // this enable the use of Symlinks
+    unstable_enablePackageExports: true,
+  },
+  // this specifies the folder where are located the node_modules for the project
+  watchFolders: [path.join(__dirname, '..', '..')],
+};
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
